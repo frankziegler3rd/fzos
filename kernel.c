c05d0c9dc7766593c7cdb578856fd1bc8f09cf9e
frankziegler3rd <frankzigs98@gmail.com>
2024-10-19 15:00:39 -0400

    Added function to clear terminal

--- kernel.c
+++ kernel.c
@@ -1,11 +1,11 @@
-void clear_terminal();
+void clear_terminal(char* VGA_BUFFER);
 
 void main() {
 
 	char* const VGA_BUFFER = (char*) 0xb8000;
 	char* str = "Hello World";
 	
-	clear_terminal();
+	clear_terminal(VGA_BUFFER);
 
 	for (int i=0; str[i] != '\0'; i++) {
 		VGA_BUFFER[i*2] = str[i];
@@ -14,7 +14,7 @@ void main() {
 	return;
 }
 
-void clear_terminal() {
+void clear_terminal(char* VGA_BUFFER) {
 	
 	for(int i = 0; i < 2000; i += 2) {
 		VGA_BUFFER[i] = ' ';
